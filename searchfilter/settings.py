583f9a457071fb81e8173de1c44ab625dc20955f
Md. Mehedi Hasan <71551839+MehediKhokon@users.noreply.github.com>
2021-03-27 20:08:56 +0600

    changes

--- searchfilter/settings.py
+++ searchfilter/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^u!**x@5sjh0z9-orgll&i9lk7w+kk#(#@rj@i@c+6%^_6e2=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
